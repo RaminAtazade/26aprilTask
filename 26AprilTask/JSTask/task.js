7e6d8bc4868d371e6a736f6543a430a5ebcb36a5
Ramin Atazade <127185803+RaminAtazade@users.noreply.github.com>
2023-04-26 10:30:18 -0700

    Add files via upload

---
+++ 26AprilTask/JSTask/task.js
@@ -0,0 +1,5 @@
+alert("Watashina Eren JEager")
+
+var randomarray = ["MentalSagliq","yuxurejimi","Yaxsiqiymetler","Sosialheyat"]
+randomarray.length = randomarray.length -1;
+console.log(randomarray)
